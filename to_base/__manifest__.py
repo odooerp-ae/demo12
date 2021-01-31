351aea87221d15c3da0e5eefb0b032d423b37218
Odooerp.ae <61090804+odooerp-ae@users.noreply.github.com>
2021-01-31 14:56:43 +0400

    Add files via upload
    test

---
+++ to_base/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Base Datetime",
+
+    'summary': """Additional tools and utilities for other modules""",
+
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': True,
+    'price': 9.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
